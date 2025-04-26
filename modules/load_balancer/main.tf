1ca64f8fcd456ac7fa747fb22ac7b24e2c0034ee
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2025-04-26 13:28:43 -0300

    Criação do modulo de Load Balancer
    Define o tipo, security gropus, subnets e tags

---
+++ modules/load_balancer/main.tf
@@ -0,0 +1,27 @@
+## Cria o load balancer
+resource "aws_lb" "this" {
+  name               = var.balancer_name
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+  enable_deletion_protection = var.enable_deletion_protection
+  enable_cross_zone_load_balancing = true
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      status_code = 200
+      content_type = "text/plain"
+      message_body = "OK"
+    }
+  }
+}
---
+++ modules/load_balancer/variables.tf
@@ -0,0 +1,37 @@
+variable "balancer_name" {
+  description = "Name of Load balancer"
+  type = string
+}
+
+variable "internal" {
+  description = "If load balancer is internal"
+  type = bool
+  default = false
+}
+
+variable "load_balancer_type" {
+  description = "Type of load balancer"
+  type = string
+}
+
+variable "security_groups" {
+  description = "List of associated security groups on the load balancer"
+  type = list(string)
+}
+
+variable "subnets" {
+  description = "List of groups where the load balancer will be created"
+  type = list(string)
+}
+
+variable "tags" {
+  description = "Tags for load balancer"
+  type = map(string)
+  default = {}
+}
+
+variable "enable_deletion_protection" {
+  description = "Habilita proteção contra exclusão"
+  type        = bool
+  default     = false
+}
