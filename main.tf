e76e969f56232bf55caa10c96817d776f97d456c
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2025-05-01 11:01:40 -0300

    style: Aplica formatação do OpenTofu

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ data "aws_iam_role" "LabRole" {
 }
 
 module "vpc" {
-    source = "./modules/vpc"
+  source = "./modules/vpc"
 
-    vpc_cidr = var.vpc_cidr
-    vpc_name = var.vpc_name
-    subnet_cidrs_public = var.subnet_cidrs_public
-    subnet_cidrs_private = var.subnet_cidrs_private
-    availability_zones = var.availability_zones
+  vpc_cidr             = var.vpc_cidr
+  vpc_name             = var.vpc_name
+  subnet_cidrs_public  = var.subnet_cidrs_public
+  subnet_cidrs_private = var.subnet_cidrs_private
+  availability_zones   = var.availability_zones
 }
 
 module "security_groups" {
@@ -24,12 +24,12 @@ module "security_groups" {
       ingress_to_port     = 80
       ingress_protocol    = "tcp"
       cidr_blocks         = ["0.0.0.0/0"]
-      security_groups = []
+      security_groups     = []
       egress_from_port    = 0
       egress_to_port      = 0
       egress_protocol     = "-1"
       egress_cidr_blocks  = ["0.0.0.0/0"]
-      tags                = {
+      tags = {
         Environment = "production"
       }
     },
@@ -47,13 +47,13 @@ module "service_security_group" {
       ingress_from_port   = 0
       ingress_to_port     = 65535
       ingress_protocol    = "tcp"
-      cidr_blocks         = []  # Empty list since we're using security_groups
-      security_groups     = [module.security_groups.security_group_ids[0]]  # Now we can reference the existing SG
+      cidr_blocks         = []                                             # Empty list since we're using security_groups
+      security_groups     = [module.security_groups.security_group_ids[0]] # Now we can reference the existing SG
       egress_from_port    = 0
       egress_to_port      = 0
       egress_protocol     = "-1"
       egress_cidr_blocks  = ["0.0.0.0/0"]
-      tags                = {
+      tags = {
         Environment = "production"
       }
     }
@@ -61,38 +61,38 @@ module "service_security_group" {
 }
 
 module "load_balancer" {
-    source              = "./modules/load_balancer"
-    vpc_id              = module.vpc.vpc_id
-    balancer_name       = var.balancer_name
-    internal            = var.internal
-    security_groups     = [module.security_groups.security_group_ids[0]]
-    subnets             = module.vpc.public_subnet_ids
-    load_balancer_type  = "application"
-    enable_deletion_protection = false
-    tags                = {
-        Environment = "production"
-    }
+  source                     = "./modules/load_balancer"
+  vpc_id                     = module.vpc.vpc_id
+  balancer_name              = var.balancer_name
+  internal                   = var.internal
+  security_groups            = [module.security_groups.security_group_ids[0]]
+  subnets                    = module.vpc.public_subnet_ids
+  load_balancer_type         = "application"
+  enable_deletion_protection = false
+  tags = {
+    Environment = "production"
+  }
 }
 
 
 module "ecs" {
-  source              = "./modules/ecs"
-  cluster_name        = "rails-tofu"
-  repository_name     = "rails-on-tofu"
-  task_family         = "rails-tofu-task"
-  execution_role_arn  = data.aws_iam_role.LabRole.arn
+  source             = "./modules/ecs"
+  cluster_name       = "rails-tofu"
+  repository_name    = "rails-on-tofu"
+  task_family        = "rails-tofu-task"
+  execution_role_arn = data.aws_iam_role.LabRole.arn
   # task_role_arn       = aws_iam_role.ecs_execution_role.arn
-  container_name      = "rails-tofu-container"
-  container_image     = var.container_image
-  rails_master_key    = var.rails_master_key
-  cpu                 = "256"
-  memory              = "512"
-  service_name        = "rails-tofu-service"
-  subnets             = module.vpc.private_subnets_ids
-  security_groups     = [module.service_security_group.security_group_ids[0]]
-  desired_count       = 2
+  container_name   = "rails-tofu-container"
+  container_image  = var.container_image
+  rails_master_key = var.rails_master_key
+  cpu              = "256"
+  memory           = "512"
+  service_name     = "rails-tofu-service"
+  subnets          = module.vpc.private_subnets_ids
+  security_groups  = [module.service_security_group.security_group_ids[0]]
+  desired_count    = 2
   target_group_arn = module.load_balancer.target_group_arn
-  tags                = {
+  tags = {
     Environment = "production"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,68 +1,68 @@
 variable "region" {
-    description = "The AWS region to deploy the resources in."
-    type        = string
-    default     = "us-east-1"
+  description = "The AWS region to deploy the resources in."
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-    description = "The CIDR block for the VPC."
-    type        = string
-    default = "10.0.0.0/24"
+  description = "The CIDR block for the VPC."
+  type        = string
+  default     = "10.0.0.0/24"
 }
 
 variable "vpc_name" {
-    description = "The name of the VPC."
-    type        = string
-    default     = "rails-vpc"
+  description = "The name of the VPC."
+  type        = string
+  default     = "rails-vpc"
 }
 
 variable "subnet_cidrs_public" {
-    description = "A list of CIDR blocks for the subnets."
-    type        = list(string)
-    default     = ["10.0.0.0/28", "10.0.0.16/28"]
+  description = "A list of CIDR blocks for the subnets."
+  type        = list(string)
+  default     = ["10.0.0.0/28", "10.0.0.16/28"]
 }
 
 variable "subnet_cidrs_private" {
-    description = "A list of CIDR blocks for the private subnets."
-    type        = list(string)
-    default     = ["10.0.0.128/28", "10.0.0.144/28"]
+  description = "A list of CIDR blocks for the private subnets."
+  type        = list(string)
+  default     = ["10.0.0.128/28", "10.0.0.144/28"]
 }
 
 variable "availability_zones" {
-    description = "A list of availability zones to use."
-    type        = list(string)
-    default     = ["us-east-1a", "us-east-1b"]
+  description = "A list of availability zones to use."
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "sg_name" {
-    description = "The name of the security group."
-    type        = string
-    default     = "rails-lb-sg"
-  
+  description = "The name of the security group."
+  type        = string
+  default     = "rails-lb-sg"
+
 }
 
 variable "balancer_name" {
   description = "Name of Load balancer"
-  type = string
-  default = "rails-lb"
+  type        = string
+  default     = "rails-lb"
 }
 
 variable "internal" {
   description = "If load balancer is internal"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "load_balancer_type" {
   description = "Type of load balancer"
-  type = string
-  default = "application"
+  type        = string
+  default     = "application"
 }
 
 variable "tags" {
   description = "Tags for load balancer"
-  type = map(string)
-  default = {}
+  type        = map(string)
+  default     = {}
 }
 
 variable "enable_deletion_protection" {
@@ -75,7 +75,7 @@ variable "rails_master_key" {
   description = "Rails master key"
   type        = string
   sensitive   = true
-  default = ""
+  default     = ""
 }
 
 variable "container_image" {
